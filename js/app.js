31b6f40156823a1baf04a6530c11c3a223ed165b
Tiffany Han <hantiffany@gmail.com>
2015-06-12 11:45:36 -0700

    Give the enemy objects unique speed values;  let the player move one block at a time within the gameboard.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,47 @@
-// Enemies our player must avoid
+// the Enemy constructor function object
+// it creates object instances that delegate their failed property
+// lookups to Enemy.prototype
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    var chooseY = function() {
-        var randomNumber = Math.random();
+    // keyword new results in:
+    // this = Object.create(Enemy.prototype);
 
-        if (0 <= randomNumber && randomNumber < .33) {
-            return 65;
-        }
-        else if (.33 <= randomNumber && randomNumber < .66) {
-            return 145;
-        }
-        else if (.66 <= randomNumber && randomNumber < 1) {
-            return 225;
-        }
-    }
+    // give the instance a random initial x position on the game board
+    this.x = Enemy.chooseX();
+    // give the instance a randomly generated speed value
+    this.speed = Enemy.chooseSpeed();
+    // give the instance its avatar
+    this.sprite = 'images/enemy-bug.png';
 
-    var chooseX = function() {
-        var randomNumber = Math.random();
-        var randomX = (randomNumber * 600)-200;
+    // keyword new results in:
+    // return this;
+}
+
+// encapsulate these functions as properties of Enemy in order to
+// move them out of the global context
+Enemy.chooseX = function() {
+        var randomX = (Math.random * 600)-200;
         return randomX;
     }
 
-    this.x = chooseX();
-    this.y = chooseY();
+Enemy.chooseY = function() {
+    var randomNumber = Math.random();
 
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    if (0 <= randomNumber && randomNumber < .33) {
+        return 65;
+    }
+    else if (.33 <= randomNumber && randomNumber < .66) {
+        return 145;
+    }
+    else if (.66 <= randomNumber && randomNumber < 1) {
+        return 225;
+    }
 }
+
+Enemy.chooseSpeed = function() {
+    var randomSpeed = (Math.floor(Math.random() * 200)) + 20;
+    return randomSpeed;
+}
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -37,12 +49,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
     if (this.x < 600) {
-        this.move = (this.x += 5) * dt;
+        this.x += 5 * this.speed * dt;
     }
     else {
         this.x = -100;
-        this.move = (this.x += 5) * dt;
+        this.y = Enemy.chooseY();
     }
 }
 
@@ -56,46 +69,42 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 
 var Player = function() {
-
-    this.x = 200;
-    this.y = 420;
+    this.x = 202.5;
+    this.y = 405;
 
     this.sprite = 'images/char-boy.png';
 }
 
 Player.prototype.update = function() {
-    var stopPoint = this.x + 80;
-
-    if (this.x < stopPoint) {
-        this.move = this.x += 5;
+// collisions and getting to the water
+/*
+    if (this.y = 100) {
+        this.x = 202.5;
+        this.y = 405;
     }
-    else {
-        this.move == this.x;
+*/
+}
+
+Player.prototype.handleInput = function() {
+    if (event.keyCode == 37 && this.x > 2.5) {
+        this.x = this.x - 100;
+    }
+
+    if (event.keyCode == 38 && this.y > -10) {
+        this.y = this.y - 83;
+    }
+
+    if (event.keyCode == 39 && this.x < 402.5) {
+        this.x = this.x + 100;
+    }
+
+    if (event.keyCode == 40 && this.y < 405) {
+        this.y = this.y + 83;
     }
 }
 
-Player.prototype.render = function(x, y) {
+Player.prototype.render = function(x,y) {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-Player.handleInput = function() {
-    if (event.keyCode == 37) {
-        alert('left was pressed');
-    }
-
-    if (event.keyCode == 38) {
-        alert('up was pressed');
-    }
-
-    if (event.keyCode == 39) {
-
-        Player.update.moveRight();
-        // this.moveRight = this.x + 80;
-    }
-
-    if (event.keyCode == 40) {
-        alert('down was pressed');
-    }
 }
 
 // Now instantiate your objects.
@@ -115,6 +124,11 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     }
 
-    Player.handleInput(allowedKeys[e.keyCode]);
-
+    // lower case because player is the variable used to name an instance of the Player class
+    // 'this' refers to the player object
+    // when you call the function as a property of Player,
+    // 'this' refers to the Player function object which doesn't make sense
+    // since you want the player object to have this functionality,
+    // not the Player function object whose job it is to construct the player instances
+    player.handleInput(allowedKeys[e.keyCode]);
 });
